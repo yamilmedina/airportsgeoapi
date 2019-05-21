82f4ac32e99222ddf9afa9628b742d8f72757359
yamil <yamilmedina@users.noreply.github.com>
2019-05-20 23:58:30 -0400

    adds controller advice configuration to handle bad request

--- src/main/java/dev/yamil/airportsgeoapi/ControllerAdvice.java
+++ src/main/java/dev/yamil/airportsgeoapi/ControllerAdvice.java
@@ -1,4 +1,24 @@
 package dev.yamil.airportsgeoapi;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
+
+@RestControllerAdvice(
+        basePackages = "dev.yamil.airportsgeoapi",
+        annotations = RestController.class)
 public class ControllerAdvice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity handleBadRequestException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Optional.of(ex.getMessage()));
+    }
+
+
 }
